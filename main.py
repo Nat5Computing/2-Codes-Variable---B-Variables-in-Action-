1fef5f44a27b1b750500a6f208cf60000d2ac8e6
MrsMillerComputing <felicity.miller@ab-ed.org>
2025-07-28 11:08:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ favouriteApp = ''
 #4. Display favourite app - concatenation example using + to join the strings. Remember if the variable stores an integer etc you would need to put it inside a string sandwich: str(nonStringVariableName)
 print('My favourite app is ' + favouriteApp)
 
-#5. Display year of birth
+# Display year of birth in a sentence: My year of birth is...
 
 
-#6. Display favourite food
+# Display favourite food in a sentence: My favourite food is...
